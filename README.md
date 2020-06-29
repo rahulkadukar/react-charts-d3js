d54178918c1bebc53c9d1943d0b115fd699776d4
Rahul Kadukar <rahulkadukar@yahoo.com>
2020-06-29 00:59:32 -0400

    Creating a basic bar chart

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 ![React](https://img.shields.io/badge/react-v16.12.0-blue)
 ![Webpack](https://img.shields.io/badge/webpack-v4.41.6-green)
 ![Styled Components](https://img.shields.io/badge/styled--components-v4.4.1-orange)
-# react-app-skeleton
-Basic React App with Logger
+
+# react-charts-d3
+Charts made in React using d3
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,5 +1,20 @@
 import React, { Component } from 'react'
-import {select, selectAll} from 'd3-selection';
+import {
+  select as d3select,
+  selectAll
+} from 'd3-selection';
+import {
+  scaleLinear as d3scaleLinear
+} from 'd3-scale';
+
+
+const data = [
+  {k: "A", v: 1},
+  {k: "B", v: 2},
+  {v: 3},
+  {v: 4},
+  {v: 5}
+]
 
 class Index extends Component {
   constructor(props) {
@@ -13,18 +28,33 @@ class Index extends Component {
 
   createD3Chart() {
     const node = this.node
+    const width = 1000
+    const height = 400
 
-    const svg = select(node)
-      .attr("width", 50)
-      .attr("height", 50)
+    const xAxisScale = d3scaleLinear()
+      .domain([0, data.length])
+      .range([0, width])
 
-    selectAll("p").style("color", "blue");
+    const svgabc = d3select(node)
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .selectAll("rect")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("width", 100)
+        .attr("height", (d, i) => d.v * 50)
+        .attr("x", (d, i) => d.v * 40)
+        .style("fill", "yellow")
+
+
   }
 
   render() {
     return (
       <div className="container" style={{marginTop: '10px'}}>
-        <svg className="d3-class" ref={node => this.node = node}></svg>
+        <div className="d3-class" ref={node => this.node = node}></div>
         <p>Random</p>
       </div>
     );
